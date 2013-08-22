b84c7388922f9e651c05fb5a36607867913e4a5d
Ville Suur-Uski <ville.suuruski@gmail.com>
2013-08-22 22:42:19 +0300

    R-file

---
+++ alkoholi/data_tutkimus.R
@@ -0,0 +1,8 @@
+miehet <- read.csv("naiset.csv", sep="\t")
+naiset <- read.csv("naiset.csv", sep="\t")
+alkoholi <- read.csv("alkoholin_kulutus.csv", sep=";")
+
+naiset$sukupuoli <- "Nainen"
+miehet$sukupuoli <- "Mies"
+
+jakauma_ika <- rbind(naiset, miehet)
