f6c871a39f0834ae8d2927b035310126b38f5391
Sampo Toiva <stoi@futurice.com>
2013-09-12 22:20:48 +0300

    First working steamgraph.

--- alkoholi/html/js/main.js
+++ alkoholi/html/js/main.js
@@ -1,6 +1,6 @@
 var config = {
-    width: 640,
-    height: 340
+    width: 1000,
+    height: 500
 };
 
 var AlcoholViz = {
@@ -18,8 +18,8 @@ var AlcoholViz = {
       "key": element.key,
       "values": element.values.map(function(ageGroup) {
         return {
-          "x": ageGroup.Vuosi,
-          "y": ageGroup.ikaluokka,
+          "x": parseInt(ageGroup.Vuosi, 10),
+          "y": parseInt(ageGroup.ikaluokka, 10),
         };
       })
     };
@@ -27,7 +27,7 @@ var AlcoholViz = {
 
   stack: function() {
     return d3.layout.stack()
-                .offset("silhouette")
+                .offset("wiggle")
                 .values(function(d) {
                   return d.values;
                 });
@@ -44,10 +44,10 @@ var AlcoholViz = {
     return d3.scale.linear().domain([1865, 2012]).range([0, config.width]);
   },
 
-  yScale: function(data){
-    var values = d3.merge(data.map(function(d){return d.values;}));
-    var min = d3.min(values, function(d) { return parseInt(d.ikaluokka,10); });
-    var max = d3.max(values, function(d) { return parseInt(d.ikaluokka,10); });
+  yScale: function(layers){
+    var values = d3.merge(layers.map(function(d){return d.values;}));
+    var min = d3.min(values, function(d) { return d.y; });
+    var max = d3.max(values, function(d) { return d.y0 + d.y; });
 
     return d3.scale.linear()
              .domain([min, max])
@@ -59,9 +59,11 @@ var AlcoholViz = {
     .x(function(d) {
       return xScale(d.x);
     })
-    .y0(config.height)
+    .y0(function(d) {
+      return yScale(d.y0);
+    })
     .y1(function(d) {
-      return yScale(d.y);
+      return yScale(d.y + d.y0);
     });
   },
 
@@ -74,9 +76,8 @@ d3.tsv("../data/jakauma.tsv", function(data) {
   var stack = AlcoholViz.stack();
   var nest = AlcoholViz.nest(data);
   var layers = stack(nest.map(AlcoholViz.initData));
-
   var xScale = AlcoholViz.xScale();
-  var yScale = AlcoholViz.yScale(nest);
+  var yScale = AlcoholViz.yScale(layers);
   var area = AlcoholViz.area(xScale, yScale);
   var color = AlcoholViz.color();
   var svg = AlcoholViz.svg();
