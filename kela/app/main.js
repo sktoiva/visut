034485f219a86399eb4f6a14bc4a152d0dfff0bd
Sampo Toiva <sampo.toiva@gmail.com>
2014-01-21 09:05:01 +0200

    Document.ready in use.

--- kela/app/main.js
+++ kela/app/main.js
@@ -1,7 +1,9 @@
 // to depend on a bower installed component:
 // define(['bower_components/componentName/file'])
 
-define(["datepicker", "timeline"], function(datepicker, timeline) {  
-  datepicker.init();
-  timeline.timeline(new Date());
+define(["datepicker", "timeline", "jquery"], function(datepicker, timeline, $) {  
+  $(document).ready(function(){
+    datepicker.init();
+    timeline.timeline(new Date());
+  });
 });
